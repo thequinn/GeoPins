b299df0e6403617f27a4f2a9f7737c9cb1de3544
thequinn <annasun.usa@gmail.com>
2020-06-27 18:03:38 -0700

    Update server.js

--- Project/server.js
+++ Project/server.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(
-    () => console.log("DB connected"),
+    () => console.log("Database connected "),
     err => console.error(err)
   );
 
